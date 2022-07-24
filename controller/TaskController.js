2996b6f9ec224ff23b9b9d9f28553a2d814db44b
Leonardo Soares <leonardoxp9d@gmail.com>
2022-07-24 17:48:28 -0300

    config index, routes and controller

---
+++ controller/TaskController.js
@@ -0,0 +1,7 @@
+const getAll = (request, response) => {
+    return response.render("index");
+};
+
+module.exports = {
+    getAll,
+};
--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 
+// responsavel por direcionar as pastas
+const path = require("path");
+const routes = require("./routes/routes");
+
 const app = express();
+const port = 3000;
 
-// Rota
-app.get('/home', (request, response) => {
-  response.send('Hello World!');
-})
+/* informa ao back-end que estamos utilizando o formato de arquivo ejs */
+app.set("view engine", "ejs");
+/* junta a pasta principal com a public 
+isso e para funcionar a renderização dos arquivos css e js no index.ejs*/
+app.use(express.static(path.join(__dirname,"public")));
+app.use(routes);
 
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
